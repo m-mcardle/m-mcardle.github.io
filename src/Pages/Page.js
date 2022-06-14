2325389f8069932f6eee49e97681fbaad1fccccb
Matt McArdle <matthew.j.mcardle@gmail.com>
2022-06-14 06:41:08 -0400

    Add analytics
    - Use insights-js

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
+import { init, trackPages } from "insights-js"
+
 import HomePage from './HomePage';
 import MagnetPage from './MagnetPage';
 import ResumePage from './ResumePage';
@@ -11,18 +13,20 @@ import { fighters } from '../Data/Fighters';
 import { food } from '../Data/FastFood';
 
 function Page() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/MagnetPage" element={<MagnetPage />} />
-          <Route path="/ResumePage" element={<ResumePage />} />
-          <Route exact path="/TierLists/F1" element={<TierListPage data={drivers} paramPage={"F1"} />} />
-          <Route exact path="/TierLists/UFC" element={<TierListPage data={fighters} paramPage={"UFC"} />} />
-          <Route exact path="/TierLists/Food" element={<TierListPage data={food} paramPage={"Food"} />} />          
-        </Routes>
-      </Router>
-    );
+  init('ozovfH7OXe0GoXN4');
+  trackPages();
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/MagnetPage" element={<MagnetPage />} />
+        <Route path="/ResumePage" element={<ResumePage />} />
+        <Route exact path="/TierLists/F1" element={<TierListPage data={drivers} paramPage={"F1"} />} />
+        <Route exact path="/TierLists/UFC" element={<TierListPage data={fighters} paramPage={"UFC"} />} />
+        <Route exact path="/TierLists/Food" element={<TierListPage data={food} paramPage={"Food"} />} />          
+      </Routes>
+    </Router>
+  );
 }
 
 export default Page;
